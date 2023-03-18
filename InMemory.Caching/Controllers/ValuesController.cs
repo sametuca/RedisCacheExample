7a5b9d65720c24897216a2969180ee63a8bcfeb9
Samet UCA <sametuca@hotmail.com>
2023-03-18 22:40:13 +0300

    delete comment

--- InMemory.Caching/Controllers/ValuesController.cs
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemory.Caching.Controllers;
 
-// The [ApiController] attribute is used to indicate that the controller responds to web API requests.
 [Route("api/[controller]")]
 [ApiController]
 public class ValuesController : ControllerBase
@@ -15,27 +14,21 @@ public class ValuesController : ControllerBase
         _memoryCache = memoryCache;
     }
 
-    // GET api/values
     [HttpGet("set/{name}")]
     public void Set(string name)
     {
-        // The Set method adds or updates a cache entry in the cache.
         _memoryCache.Set("name", name);
     }
     
-    // GET api/values
     [HttpGet("get")]
     public string? Get()
     {
-        // If the key is not found, the method returns false and the value parameter contains the default value for the type of the value parameter.
         return _memoryCache.TryGetValue("name", out string? name) ? name : "Not Found";
     }
 
-    // GET api/values
     [HttpGet("set-date")]
     public void SetDate()
     {
-        // The Set method adds or updates a cache entry in the cache.
         _memoryCache.Set<DateTime>("date",DateTime.Now,options:new()
         {
             AbsoluteExpiration = DateTime.Now.AddSeconds(30),
@@ -43,11 +36,9 @@ public class ValuesController : ControllerBase
         });
     }
     
-    // GET api/values
     [HttpGet("get-date")]
     public DateTime GetDate()
     {
-        // If the key is not found, the method returns false and the value parameter contains the default value for the type of the value parameter.
         return _memoryCache.Get<DateTime>("date");
     }
 }
